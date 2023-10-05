3a469b6188398356e0905f6fe9f9f334ab73e591
Piotr Jaworski <piotr.jaworski@rtbhouse.com>
2023-10-05 09:39:45 +0200

    initial commit

---
+++ modules/rtbhousePubvertiserRtdProvider.js
@@ -0,0 +1,46 @@
+import {isStr, logError} from '../src/utils.js';
+import {submodule} from '../src/hook.js';
+import { loadExternalScript } from '../src/adloader.js';
+
+const MODULE_NAME = 'rtbhousePubvertiser';
+const TAGDOMAIN = 'tags.creativecdn.com';
+const RTBH_EVENTS = 'rtbhEvents';
+
+window[RTBH_EVENTS] = window[RTBH_EVENTS] || [];
+
+function executePubvertiserTag(tagId, region) {
+  (function (w, d, dn, t) {
+    w[dn] = w[dn] || [];
+    w[dn].push({ eventType: 'init', value: t, dc: region });
+    const scriptUrl = `https://${TAGDOMAIN}/${tagId}.js`;
+    loadExternalScript(scriptUrl, MODULE_NAME);
+  })(window, document, RTBH_EVENTS, tagId);
+  window[RTBH_EVENTS].push(
+    {
+      eventType: 'placebo',
+    }
+  );
+}
+
+function init(rtdConfig) {
+  const { tagId, region = '' } = rtdConfig?.params;
+
+  if (!tagId || !isStr(tagId) || !isStr(region)) {
+    logError(`${MODULE_NAME}: params.tagId and params.region should be strings`);
+    return false;
+  }
+
+  executePubvertiserTag(tagId, region);
+  return true;
+}
+
+const rtbhousePubvertiserRtdModule = {
+  name: MODULE_NAME,
+  init: init
+};
+
+function registerSubModule() {
+  submodule('realTimeData', rtbhousePubvertiserRtdModule);
+}
+
+registerSubModule();
---
+++ modules/rtbhousePubvertiserRtdProvider.md
@@ -0,0 +1,3 @@
+# RTB House Pubvertiser Real-time Data Submodule
+This module is intended to be used by RTB House (https://rtbhouse.com) partners and puts pubvertiser tagging script
+onto publisher's web page.
